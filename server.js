e61fcdddcc8cf98c8749d527331728d30e71d83c
Fgergo20 <74962087+Fgergo20@users.noreply.github.com>
2025-08-09 21:59:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,20 +7,41 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-    console.log("New player:", socket.id);
+let players = {}; // játékosok pozíciói
 
+io.on("connection", (socket) => {
+    console.log("Új játékos:", socket.id);
+
+    // új játékos kezdő pozíció
+    players[socket.id] = { x: 100, y: 100, color: getRandomColor() };
+
+    // elküldjük neki az összes játékost
+    socket.emit("currentPlayers", players);
+
+    // mindenki máshoz is elküldjük az új játékost
+    socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
+
+    // mozgás frissítés
     socket.on("move", (data) => {
-        // broadcast player movement to others
-        socket.broadcast.emit("playerMoved", { id: socket.id, ...data });
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            io.emit("playerMoved", { id: socket.id, x: data.x, y: data.y });
+        }
     });
 
+    // lelépés
     socket.on("disconnect", () => {
-        console.log("Player disconnected:", socket.id);
+        console.log("Játékos lelépett:", socket.id);
+        delete players[socket.id];
         io.emit("playerLeft", socket.id);
     });
 });
 
 http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Szerver fut a ${PORT} porton`);
 });
+
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
